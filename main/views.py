dfc9105d8ce46b40963d42914ded9887af7a82ff
ze1net <zafarbekergashov0706@gmail.com>
2022-10-10 13:26:09 +0500

    news 3 commit

--- main/views.py
+++ main/views.py
@@ -7,7 +7,7 @@ def MainPageView(request):
     news = NewsModel.objects.all()
     search = request.GET.get('q', '')
     if search:
-        news = news.filter(Q(body__icontains=search) | Q(title__icontains=search))
+        news = news.filter(Q(body__icontains=search) | Q(title__icontains=search) | Q(category__icontains=search))
         return render(request, 'pages/main_page.html', context={
             'news': news
         })
@@ -16,14 +16,7 @@ def MainPageView(request):
     })
 
 
-def NewsSearchView(request):
-    if request.method == 'News':
-        searched = request.News['searched']
-    news = NewsModel.objects.filter(title__icontains=searched)
-    return render(request,
-                  'pages/searchpage.html', context={
-             'news': news
-        })
+
 def WorldNewsView(request):
     news = NewsModel.objects.all()
     return render(request, 'pages/worldnews_page.html', context={
@@ -33,4 +26,9 @@ def UzbNewsView(request):
     news = NewsModel.objects.all()
     return render(request, 'pages/uzbnews_page.html', context={
         'news': news
+    })
+def NewsDetailView(request):
+    new = NewsModel.objects.get()
+    return render(request, 'pages/news_detail.html', context={
+        'new': new
     })
